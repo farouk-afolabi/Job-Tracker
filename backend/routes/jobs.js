d55e85f623bd0bc8bf45cfe731a60e04b93b803d
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-07-08 11:04:51 -0400

    created authentication and jobs routes

---
+++ backend/routes/jobs.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getJobs,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require('../controllers/jobs');
+const { protect } = require('../middleware/auth');
+
+router.route('/').get(protect, getJobs).post(protect, createJob);
+router.route('/:id').put(protect, updateJob).delete(protect, deleteJob);
+
+module.exports = router;
