eb64b6e406831f783737737ddde363259dcb6814
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-07-08 09:45:12 -0400

    setup server.js and db.js in backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/errorHandler');
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Job Tracker API is running!');
+  });
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/jobs', require('./routes/jobs'));
+
+// Error handling
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
