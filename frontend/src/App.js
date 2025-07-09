aa4e819cd7b1f1f3b58c2dd02f36172e4002793e
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-07-09 15:01:14 -0400

    created Login & register component and also updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,57 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getJobs } from './features/jobs/jobSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
-import JobList from './components/JobList';
+import Dashboard from './components/Dashboard';  
+import Login from './components/Login';     
+import Register from './components/Register';  
+import JobForm from './components/JobForm';
+import JobSearch from './components/JobSearch';
 import Spinner from './components/Spinner';
+import { getJobs, reset as resetJobs } from './features/jobs/jobSlice';
+import { reset as resetAuth } from './features/auth/authSlice';
 
 function App() {
   const dispatch = useDispatch();
-  const { jobs, isLoading } = useSelector((state) => state.jobs);
+  const { user } = useSelector((state) => state.auth);
+  const { isLoading } = useSelector((state) => state.jobs);
 
+  // Clear all states when component unmounts
   useEffect(() => {
-    dispatch(getJobs());
+    return () => {
+      dispatch(resetJobs());
+      dispatch(resetAuth());
+    };
   }, [dispatch]);
 
+  // Load jobs when user is authenticated
+  useEffect(() => {
+    if (user) {
+      dispatch(getJobs());
+    }
+  }, [user, dispatch]);
+
   return (
-    <div className="app">
-      <Header />
-      <main>
-        {isLoading ? <Spinner /> : <JobList jobs={jobs} />}
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <main>
+          {isLoading && <Spinner />}
+          <ToastContainer position="top-right" autoClose={3000} />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/add-job" element={<JobForm />} />
+            <Route path="/edit-job/:id" element={<JobForm />} />
+            <Route path="/job-search" element={<JobSearch />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
